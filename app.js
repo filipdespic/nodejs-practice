5217b3cac9f1b172b6f70b774654636a2dc69c31
filipdespic <despicpb12@gmail.com>
2023-12-12 18:42:35 +0100

    add middleware

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+});
 
-server.listen(3000);        
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Add Product</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Any path</h1>');
+});
+
+app.listen(3000);        
