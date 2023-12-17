ee7944f2d4e02a62e4e38d82b6569d382a33ecac
filipdespic <despicpb12@gmail.com>
2023-12-18 00:23:23 +0100

    add Router

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Add Product</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Any path</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+
 
 app.listen(3000);        
