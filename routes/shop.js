1051419ce13f2820a95cfdadc24eedd38380812a
filipdespic <despicpb12@gmail.com>
2023-12-18 09:56:14 +0100

    add helper for path

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDit = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDit, 'views', 'shop.html'));
 });
 
 module.exports = router;
